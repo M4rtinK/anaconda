12491874049f62a2cc19b1fe58f79d1af7832710
Martin Kolman <mkolman@redhat.com>
2014-12-16 16:07:02 +0100

    Replace python-urlgrabber with python-requests (#1141242)
    Python urlgrabber is Python 2 only and kinda crufty.
    Replace it with the Python 3 compatible python-requests module,
    which provides a much nicer interface, is in active development
    and is very widely used.

--- pyanaconda/packaging/__init__.py
+++ pyanaconda/packaging/__init__.py
@@ -31,6 +31,7 @@ import os
 import requests
 import configparser
 import shutil
+import time
 from glob import glob
 from fnmatch import fnmatch
 import threading
@@ -68,8 +69,7 @@ import blivet.arch
 from blivet.platform import platform
 
 from pyanaconda.product import productName, productVersion
-import urlgrabber
-urlgrabber.grabber.default_grabber.opts.user_agent = "%s (anaconda)/%s" %(productName, productVersion)
+USER_AGENT = "%s (anaconda)/%s" %(productName, productVersion)
 
 from distutils.version import LooseVersion
 
@@ -340,9 +340,6 @@ class Payload(object):
         log.debug("retrieving treeinfo from %s (proxy: %s ; sslverify: %s)",
                   url, proxy_url, sslverify)
 
-        ugopts = {"ssl_verify_peer": sslverify,
-                  "ssl_verify_host": sslverify}
-
         proxies = {}
         if proxy_url:
             try:
@@ -353,21 +350,26 @@ class Payload(object):
                 log.info("Failed to parse proxy for _getTreeInfo %s: %s",
                          proxy_url, e)
 
-        ug = URLGrabber()
+        response = None
+        headers = {"user-agent": USER_AGENT}
         try:
-            treeinfo = ug.urlgrab("%s/.treeinfo" % url,
-                                  "/tmp/.treeinfo", copy_local=True,
-                                  proxies=proxies, **ugopts)
-        except URLGrabError as e:
+            response = requests.get("%s/.treeinfo" % url, headers=headers, proxies=proxies, verify=sslverify)
+        except requests.exceptions.RequestException as e:
             try:
-                treeinfo = ug.urlgrab("%s/treeinfo" % url,
-                                      "/tmp/.treeinfo", copy_local=True,
-                                      proxies=proxies, **ugopts)
-            except URLGrabError as e:
+                response = requests.get("%s/treeinfo" % url, headers=headers, proxies=proxies, verify=sslverify)
+            except requests.exceptions.RequestException as e:
                 log.info("Error downloading treeinfo: %s", e)
-                treeinfo = None
+                response = None
 
-        return treeinfo
+        if response:
+            # write the the local treeinfo file
+            with open("/tmp/.treeinfo", "w") as f:
+                f.write(response.text())
+
+            # and also return the treeinfo contents as a string
+            return response.text
+        else:
+            return None
 
     def _getReleaseVersion(self, url):
         """ Return the release version of the tree at the specified URL. """
